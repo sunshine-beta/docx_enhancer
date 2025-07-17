d7230d1957f5355bd708e206d97eccc680107c2a
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-17 20:26:58 +0500

    refactor: mongoose config

--- backend/config/mongoose.connection.js
+++ backend/config/mongoose.connection.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-// destructure MONGODB_URI from environment variables
-const { MONGODB_URI } = process.env;
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
