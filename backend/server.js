3a217945244532e101ea2a07adc73a06a580997e
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-18 04:39:23 +0500

    fix: cors url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://backend-production-8568.up.railway.app",
+    origin: "https://docx-app-frontend-production.up.railway.app/",
   })
 );
 
